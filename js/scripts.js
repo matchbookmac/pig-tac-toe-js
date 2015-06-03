cf6d31b908daa5297b0233754cd8bb6e2334dc98
David Garber & Steve Schneider <student@epicodus.com>
2015-06-03 10:51:51 -0700

    let a player mark the space.

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,12 @@ function Player(playerSymbol) {
   this.mark = playerSymbol;
 }
 
-function Space(coordinateX, coordinateY) {
+function Space(coordinateX, coordinateY, markedBy) {
   this.x_coordinate = coordinateX;
   this.y_coordinate = coordinateY;
+  this.markedBy = markedBy;
 }
+
+Space.prototype.mark_by = function(player) {
+  this.markedBy = player;
+};
--- spec/specs.js
+++ spec/specs.js
@@ -7,12 +7,19 @@ describe('Player', function() {
 
 describe('Space', function() {
   it("returns the player's mark at an x coordinate", function() {
-    var testSpace = new Space(1,2);
+    var testSpace = new Space(1, 2, "");
     expect(testSpace.x_coordinate).to.equal(1);
   });
 
   it("returns the player's mark at a y coordinate", function() {
-    var testSpace = new Space(1,2);
+    var testSpace = new Space(1, 2, "");
     expect(testSpace.y_coordinate).to.equal(2);
+  });
+
+  it("lets a player mark the space", function() {
+    var testPlayer = new Player("X");
+    var testSpace = new Space(1, 2, "");
+    testSpace.mark_by(testPlayer)
+    expect(testSpace.markedBy).to.equal(testPlayer);
   });
 });

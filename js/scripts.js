9d435f4331f9205b0b3e6465c2648c9dc85ea723
David Garber & Steve Schneider <student@epicodus.com>
2015-06-03 09:45:25 -0700

    return the player's mark at a coordinate.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
 function Player(playerSymbol) {
   this.mark = playerSymbol;
 }
+
+function Space(coordinateX, coordinateY) {
+  this.x_coordinate = coordinateX;
+}
--- spec/specs.js
+++ spec/specs.js
@@ -4,3 +4,10 @@ describe('Player', function() {
     expect(testPlayer.mark).to.eql("X");
   });
 });
+
+describe('Space', function() {
+  it("returns the player's mark at a coordinate", function() {
+    var testSpace = new Space(1,2);
+    expect(testSpace.x_coordinate).to.equal(1);
+  });
+});

7cf46bde1a5af11fb3597315c959f45a48eaa647
David Garber & Steve Schneider <student@epicodus.com>
2015-06-03 09:47:43 -0700

    add y coordinate spec and code to pass

--- js/scripts.js
+++ js/scripts.js
@@ -4,4 +4,5 @@ function Player(playerSymbol) {
 
 function Space(coordinateX, coordinateY) {
   this.x_coordinate = coordinateX;
+  this.y_coordinate = coordinateY;
 }
--- spec/specs.js
+++ spec/specs.js
@@ -6,8 +6,13 @@ describe('Player', function() {
 });
 
 describe('Space', function() {
-  it("returns the player's mark at a coordinate", function() {
+  it("returns the player's mark at an x coordinate", function() {
     var testSpace = new Space(1,2);
     expect(testSpace.x_coordinate).to.equal(1);
+  });
+
+  it("returns the player's mark at a y coordinate", function() {
+    var testSpace = new Space(1,2);
+    expect(testSpace.y_coordinate).to.equal(2);
   });
 });
